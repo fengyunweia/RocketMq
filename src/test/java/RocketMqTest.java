72f764841e28e04a3036f5da06edf434d5cb780d
fengyunweia <15139637232@163.com>
2023-03-03 10:09:35 +0800

    add:rocketmq 保证 消息可靠性解读

--- src/test/java/RocketMqTest.java
+++ src/test/java/RocketMqTest.java
@@ -29,6 +29,44 @@ class RocketMqTest {
 
     @Test
     public void send() throws InterruptedException, MQBrokerException, RemotingException, MQClientException {
+
+        /**
+         * rocketMq 如何保证消息的可靠性
+         * 生产者：
+         * 同步发送 会等待发送信息的返回结果 只有返回了SEND_OK 才是发送成功
+         * 异步发送 生产者发送有 成功和失败的回调函数 ，（回调函数是在另一个线程中执行）也是只有返回的了SEND_OK才是发送成功，
+         * 上面两个如果返回失败 则进入发送重试，根据相应的策略（messageDelayLevel16个等级）进行重试
+         * 如果如果消费满16次之后还是未能消费成功，则不再重试，会将消息发送到死信队列（人工Console 控制界面干预），从而保证消息存储的可靠性。
+         * 单向发送 如果想要保证消息可靠性，建议不适用这种方法
+         *
+         * 储存端(broker)消息可靠性保证
+         * ConsumeQueue: CommitLog里存储了Consume Queues 、Message Key、Tag等所有信息
+         * ，即使ConsumeQueue丢失，也可以通过 commitLog完全恢复出来，这样只要保证commitLog数据的可靠性，就可以保证Consume Queue的可靠性。
+         *
+         * commintLog 可靠性保证
+         * RocketMQ存储端也即Broker端在存储消息的时候会面临以下的存储可靠性挑战：
+         *
+         * 过期文件删除
+         * 为了不让硬盘被过期消息占满，导致消息不能持久化到硬盘 过期删除一定文件，使硬盘容量一直保持再一个水平上
+         *
+         * 1：Broker正常关闭
+         * 2：Broker异常Crash
+         * 3：OS Crash
+         * 4：机器掉电，但是能立即恢复供电情况
+         * 5：机器无法开机（可能是cpu、主板、内存等关键设备损坏）
+         * 6：磁盘设备损坏
+         *
+         * 1：正常关闭 broker可以通过正常启动回复所有数据 234同步刷盘（即有一条消息进来就立刻存储） 56单点故障可以设置slave节点
+         * 主从异步复制仍然可能有极少量数据丢失，同步复制可以完全避免单点问题。
+         *
+         * 消费者消息可靠性
+         * 通常消费消息的确认机制一般分为两种思路
+         * 1：先提交后消费
+         * 2：先消费，消费成功后再提交
+         * 思路1可以解决重复消费的问题但是会丢失消息，因此RocketMQ默认实现的是思路2，由各自consumer业务方保证幂等来解决重复消费问题。
+         *
+         *
+         */
 /*        //同步发送 对结果很关注
         for (int i=0;i<=100;i++){
             SendResult sendResult = template.syncSend("fengyunweiTopic",String.valueOf(i));
