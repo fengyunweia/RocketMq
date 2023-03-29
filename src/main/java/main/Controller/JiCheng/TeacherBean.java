f05a406dc4de70b29eccb2acc332d23e32ab857f
fengyunweia <15139637232@163.com>
2023-03-29 17:03:56 +0800

    add:redis单线程原因，以及io基础知识和i/o多路复用

---
+++ src/main/java/main/Controller/JiCheng/TeacherBean.java
@@ -0,0 +1,29 @@
+package main.Controller.JiCheng;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+
+/**
+ * @author fengyunwei
+ */
+public class TeacherBean implements BeanNameAware, BeanFactoryAware, InitializingBean, DisposableBean {
+
+    @Override
+    public void setBeanName(String s) {
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+    @Override
+    public void destroy() throws Exception {
+
+    }
+}
