f5b8a57592e4b9000a613c24645f1f97553046e9
fengyunwei <15139637232@163.com>
2023-06-02 10:16:36 +0800

    add:乐观锁 悲观锁 select for update

--- src/test/java/SynchronizedTest.java
+++ src/test/java/SynchronizedTest.java
@@ -3,6 +3,19 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class SynchronizedTest {
+    /***
+     * 锁代码块{
+     * 锁 this ：锁本实例对象
+     * 锁 .class 锁这个类
+     * 锁 object 一个实例对象 相当于this
+     * }
+
+     *
+     * 锁方法{
+     *     锁这个实例的方法
+     *     加上 static  锁这个类的这个方法
+     * }
+     */
 
     static synchronized void test1(){
         int a =5;
