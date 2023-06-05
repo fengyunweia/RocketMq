cc04c990b70ee0e0ab5fe70e455ca24a7d2473e8
fengyunwei <15139637232@163.com>
2023-06-05 11:58:12 +0800

    add:ceshi

--- src/main/java/main/Controller/JiCheng/testMain.java
+++ src/main/java/main/Controller/JiCheng/testMain.java
@@ -4,7 +4,7 @@ public class testMain {
     public static void main(String[] args) {
 
     }
-    public void test(String a ,String b,String c){
+    public void test(String a ,String b,String c,String d){
 
     }
 }
